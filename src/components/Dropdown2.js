eec1b48a3123539cf2df7c3b158c171b1663e4be
AdaN6 <a_disney_da@hotmail.com>
2022-11-09 20:05:03 +0100

    created first edition of navBar and submenu dropdown

--- src/components/Dropdown2.js
+++ src/components/Dropdown2.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import { Link; } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import "./header.css"
 
 const Dropdown2 = () => {
   return (
-    <div>
-        <ul>
-            <li>
-                <Link>
-                </Link>
-
-            </li>
-        </ul>
-      
+    <div class="recipe_header">
+      <ul>
+        <li>
+          <Link to="seafood">Seafood</Link>
+        </li>
+        <li>
+          <Link to="maindish">Maindish</Link>
+        </li>
+        <li>
+          <Link to="seafood">Seafood</Link>
+        </li>
+        <li>
+          <Link to="dessert">Dessert</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Dropdown2
--- src/components/Navbar_basic.js
+++ src/components/Navbar_basic.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import "./header.css"
+import Dropdown2 from "./Dropdown2";
 
 const Navbar_basic = () => {
   return (
     <div>
-      
+      <ul class="recipe_header">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li className="category_button">
+          <NavLink to="#">Category</NavLink>
+            <ul className="submenu">
+            <li><Dropdown2 /></li>
+
+            </ul>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Navbar_basic
